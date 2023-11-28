bab0b673a0e1a9ead73135dc0039b0f3838ac9a8
SeymurAliyeev <seymuraa@code.edu.az>
2023-11-28 18:05:28 +0400

    first commit

---
+++ ATM case/ATM case/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("Enter UserName!");
+string UserName = Console.ReadLine();
+int Balance = 1000;
+if (UserName == "Seymur")
+{
+    Console.WriteLine("Enter Password");
+}
+else
+{
+    Console.WriteLine("Incorrect Username. Please, enter available Username!");
+}
+string Password = Console.ReadLine();
+if (Password == "seymur123")
+{
+    Console.WriteLine("Welcome!");
+}
+else
+{
+    Console.WriteLine("Invalid Password. Please, enter correct Password!");
+}
+Console.WriteLine("1. Balance");
+Console.WriteLine("2. Cash in");
+Console.WriteLine("3. Cash out");
+Console.WriteLine("4. Quit");
+string Option = Console.ReadLine();
+if (Option == "1")
+{
+    Console.WriteLine("Current Balance:" + Balance);
+    Console.ReadLine();
+
+}
+else if (Option == "2")
+{
+    Console.WriteLine("Please, enter the amount you want to put into your balance" + ("--Current Balance:" + Balance));
+    int amount = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Current Balance:" + (Balance + amount));
+    Console.ReadLine();
+}
+else if (Option == "3")
+{
+    Console.WriteLine("Please, enter the amount you want to take out from your balance" + ("--Current Balance" + Balance));
+    int amount = Convert.ToInt32(Console.ReadLine());
+    if (amount <= Balance)
+    {
+        Console.WriteLine("Current Balance:" + (Balance - amount));
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("You can not take higher amount than your balance!");
+    }
+}
+else if (Option == "4")
+{
+    Console.WriteLine("Thanks for using our service.");
+    Console.WriteLine("Have a nice day!");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Please, choose one of available options!");
+    Console.ReadLine();
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+//Task 1
+
+string month = "July";
+
+if ( month == "December" || month == "January" || month == "February")
+{
+    Console.WriteLine("Winter");
+}
+else if ( month == "March" || month == "April" || month == "May")
+{
+    Console.WriteLine("Spring");
+}
+else if ( month == "June" || month == "July" || month == "August")
+{
+    Console.WriteLine("Summer");
+}
+else if ( month == "September" || month == "October" || month == "November")
+{
+    Console.WriteLine("Autumn");
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+
+
+
+
